2911128bfe936df62ca74349fc758827801f4c06
Ferrisx4 <ferrisx4@gmail.com>
2020-08-19 10:05:34 -0400

    Handle mRNA field on config page

--- js/gene_jbrowse_field.js
+++ js/gene_jbrowse_field.js
@@ -3,8 +3,8 @@ var subs = new Object();
     subs["genus"]           = "fraxinus";
     subs["species"]         = "excelsior";
     subs["common name"]     = "European ash";
-    subs["coords"]          = "71 42";
-    //subs["child sequence name"] = "some child";
+    subs["coords"]          = "226460..236641";
+    subs["child sequence name"] = "OFAS011230-RA";
     subs["gene name"]       = "OFAS019435";
     subs["analysis name"]   = "blue analysis";
     subs["g"]               = "f";
@@ -23,10 +23,14 @@ if (url.includes("gene_jbrowse_field/config"))
 {
     jQuery(document).ready(function ($){
         // Are we on the gene_jbrwose_field config page?
-        // TODO: this
+        // TODO: make this more flexible to support n number of fields
         main($);
         // Update the link and preview on text field change
         $('#url_template_field').on('input',function(e){
+            main($);
+        });
+        // Same for the mRNA field
+        $('#mrna_url_template_field').on('input',function(e){
             main($);
         });
     });
@@ -35,14 +39,21 @@ if (url.includes("gene_jbrowse_field/config"))
 // Main (the meat and potatoes)
 function main($)
 {
+    // Gene
     // Get the new value
-    link_value = $('#url_template_field').val()
-
+    gene_link_value = $('#url_template_field').val()
     // Run the string through the formatter
-    link_value = template_formatter(link_value);
-
+    gene_link_value = template_formatter(gene_link_value);
     // Finally, do the update
-    update_link(link_value,$);
+    update_link('gene', gene_link_value,$);
+
+    // Same for mRNA
+    // Get the new value
+    mrna_link_value = $('#mrna_url_template_field').val()
+    // Run the string through the formatter
+    mrna_link_value = template_formatter(mrna_link_value);
+    // Finally, do the update
+    update_link('mrna',mrna_link_value,$);
 }
 
 
@@ -63,10 +74,11 @@ function template_formatter(link_value)
 }
 
 // Update the preview link
-function update_link(link_value,$)
+function update_link(field, link_value,$)
 {
     // Change the link text
-    $("#jbrowse_preview_link").text(link_value);
+    id = "#jbrowse_preview_link_" + field;
+    $(id).text(link_value);
     // Change the link itself
-    $("#jbrowse_preview_link").attr("href", link_value);
+    $(id).attr("href", link_value);
 }
